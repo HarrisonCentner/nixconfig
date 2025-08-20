7e0a2521a5b2e7caa35eabe0c72dee1552ea6b67
HarrisonCentner <harrison.centner@gmail.com>
2025-08-19 21:35:20 -0700

    services

--- machines/zylphia.nix
+++ machines/zylphia.nix
@@ -1,7 +1,6 @@
 { config, pkgs, username, homeDirectory, ... }:
 {
 
-  services.openssh.enable = true;
 
   users.users.${username} = {
     isNormalUser = true;
@@ -27,69 +26,9 @@
     nerd-fonts.droid-sans-mono
   ];
 
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/vdb";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted";
-                settings = {
-                  allowDiscards = true;
-                  bypassWorkqueues = true;
-                };
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "/swap" = {
-                      mountpoint = "/.swapvol";
-                      swap.swapfile.size = "32GB";
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
+  imports = [
+    zylphia/disko.nix
+    zylphia/services.nix
+  ];
+
 }
