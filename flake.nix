afef04e5b2907828466fd9fd4177c51377df03d5
HarrisonCentner <harrison.centner@gmail.com>
2025-11-30 00:17:28 -0800

    rm dead code

--- flake.nix
+++ flake.nix
@@ -48,42 +48,4 @@
       ];
       _module.args.rootPath = ./.;
     };
-
-#   {
-#     nixosConfigurations.zylphia = 
-#     let
-#       username = "hcentner";
-#       homeDirectory = "/home/${username}";
-#       domain-name = "hcentner.com";
-#       system = "x86_64-linux";
-#     in
-#     nixpkgs.lib.nixosSystem {
-#       system = system;
-#       specialArgs = {
-#         inherit system username homeDirectory hcentner-blog; 
-#       };
-#       modules = [
-#         disko.nixosModules.disko
-#         ./machines/zylphia/default.nix
-#         home-manager.nixosModules.home-manager
-#         { home-manager = import ./home-manager/common.nix { inherit username homeDirectory; }; }
-#         (import ./machines/common.nix { inherit username homeDirectory; })
-#       ];
-#     };
-
-#     darwinConfigurations.xlthlx = 
-#     let
-#       username = "harrisoncentner";
-#       homeDirectory = "/Users/${username}";
-#     in
-#     nix-darwin.lib.darwinSystem {
-#       system = "x86_64-darwin"; 
-#       modules = [
-#         ./machines/xlthlx.nix
-#         home-manager.darwinModules.home-manager 
-#         { home-manager = import ./home-manager/common.nix { inherit username homeDirectory; }; }
-#         (import ./machines/common.nix { inherit username homeDirectory; })
-#       ];
-#     };
-#   };
 }
