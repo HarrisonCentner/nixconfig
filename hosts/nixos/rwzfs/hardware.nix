2ea5fd3da5cc34f533ed812c7f1fa86b93203681
HarrisonCentner <harrisoncent@proton.me>
2025-12-07 21:47:43 -0500

    reorder hardware config

--- hosts/nixos/rwzfs/hardware.nix
+++ hosts/nixos/rwzfs/hardware.nix
@@ -1,15 +1,16 @@
 {
   flake.nixosModules."rwzfs-hardware" = { pkgs, lib, config, ...}: {
 
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
     boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
     boot.kernelModules = [ "kvm-intel" "iwlwifi" ];
     boot.extraModulePackages = [ ];
-    
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.enableRedistributableFirmware = true;
-    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     hardware.firmware = with pkgs; [ linux-firmware ];
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    
 
     networking.firewall.enable = false; 
     networking.nameservers = [ "1.1.1.1" ]; 
