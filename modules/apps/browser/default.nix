9df408a3d1141bcc482ae262463553f1b4f17508
HarrisonCentner <harrisoncent@proton.me>
2025-12-24 17:32:37 -0500

    notes for installing extensions on ungoogled-chromium

--- modules/apps/browser/default.nix
+++ modules/apps/browser/default.nix
@@ -7,4 +7,12 @@
       ungoogled-chromium
     ];
   };
+
+  # Install extensions on ungoogled-chromium
+  # 
+  #  1. Download the latest release from chromium-web-store [here](https://github.com/NeverDecaf/chromium-web-store).
+  #  2. Install the extension as a `.crx` (you might need to download the `.tar` extension and unpack it).
+  #  3. Go to the chromium web store and add the required extension.
+  #
+  #  See extended instructions [here](https://avoidthehack.com/manually-install-extensions-ungoogled-chromium).
 }
