f06ee8177956d2f2a85c25bbb3a7ba24aa9aaee1
HarrisonCentner <harrisoncent@proton.me>
2026-03-20 11:27:56 -0400

    use miso cachix

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -25,9 +25,11 @@ let
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+            "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
           ];
           substituters = [
             "https://cache.iog.io"
+            "https://miso-haskell.cachix.org"
           ];
           download-buffer-size = 524288000;
           allow-import-from-derivation = true;
