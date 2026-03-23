ac9d5179f1b4b016b35abaf4529ac69e5e3bc610
HarrisonCentner <harrisoncent@proton.me>
2026-03-23 10:43:39 -0400

    add zsh aliases

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -6,10 +6,12 @@
         enable = true;
         shellAliases = {
           claude = "claude-bun";
+          nsp = "nix-shell -p $@ --command zsh";
           q = "exit";
           tree = "ls --tree";
           v = "vim -u $HOME/.vimrc";
           vim = "vim -u $HOME/.vimrc";
+          claudius = "claude-bun --dangerously-skip-permissions";
         };
         oh-my-zsh = {
           enable = true;
