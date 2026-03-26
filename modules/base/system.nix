26f4704a760b15aa40456451c47ce73c79e2133e
HarrisonCentner <harrisoncent@proton.me>
2026-03-26 13:48:15 -0400

    update nix config

--- modules/base/system.nix
+++ modules/base/system.nix
@@ -1,6 +1,9 @@
 {
   flake.modules = {
-    nixos.base.system.stateVersion = "25.11";
+    nixos.base.system = {
+      stateVersion = "25.11";
+      switch.enable = true;
+    };
     darwin.base.system.stateVersion = 5;
     homeManager.base.home.stateVersion = "24.11";
   };
