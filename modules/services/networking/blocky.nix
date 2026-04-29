25bdaaac627699486f37be2d50affb44b44f7047
HarrisonCentner <harrisoncent@proton.me>
2026-04-29 09:51:51 -0400

    remove denylists

--- modules/services/networking/blocky.nix
+++ modules/services/networking/blocky.nix
@@ -1,6 +1,5 @@
 let
   dnsPort = 53; # port for incoming DNS Queries.
-  stephenBlackUrl = "https://raw.githubusercontent.com/StevenBlack/hosts/master";
 in
 {
   flake.modules.nixos.blocky =
@@ -29,28 +28,6 @@ in
                 "1.1.1.1"
                 "1.0.0.1"
               ];
-            };
-            #Enable blocking of certain domains.
-            blocking = {
-              denylists = {
-                ads = [ "${stephenBlackUrl}/hosts" ];
-                fakenews = [ "${stephenBlackUrl}/alternates/fakenews-only/hosts" ];
-                gambling = [ "${stephenBlackUrl}/alternates/gambling-only/hosts" ];
-                porn = [ "${stephenBlackUrl}/alternates/porn-only/hosts" ];
-                social = [ "${stephenBlackUrl}/alternates/social-only/hosts" ];
-              };
-              #Configure what block categories are used
-              clientGroupsBlock = {
-                default = [
-                  "ads"
-                  "porn"
-                ];
-                iphone = [
-                  "ads"
-                  "porn"
-                  "social"
-                ];
-              };
             };
             caching = {
               minTime = "5m";
