b59ef0925ba49c029b536dc8ed31256529648eab
HarrisonCentner <harrisoncent@proton.me>
2026-07-14 12:50:28 -0400

    change emoji font

--- modules/shell/terminal/fonts.nix
+++ modules/shell/terminal/fonts.nix
@@ -1,8 +1,30 @@
 {
   flake.modules.nixos.shell =
     { pkgs, ... }:
+    let
+      apple-color-emoji = pkgs.stdenvNoCC.mkDerivation {
+        pname = "apple-color-emoji";
+        version = "macos-26-20260613";
+        src = pkgs.fetchurl {
+          url = "https://github.com/samuelngs/apple-emoji-ttf/releases/download/macos-26-20260613-f1fc560b/AppleColorEmoji-Linux.ttf";
+          hash = "sha256-uMjtl/ZCuJuko2o+CWYZ8IBdBswlrhEW5pU7mBQq4gw=";
+        };
+        dontUnpack = true;
+        installPhase = "install -Dm444 $src $out/share/fonts/truetype/AppleColorEmoji.ttf";
+      };
+    in
     {
-      fonts.fontconfig.enable = true;
+      fonts = {
+        fontconfig = {
+          enable = true;
+          defaultFonts.emoji = [ "Apple Color Emoji" ];
+        };
+        packages = [
+          pkgs.noto-fonts-cjk-sans
+          pkgs.noto-fonts-cjk-serif
+          apple-color-emoji
+        ];
+      };
     };
 
   flake.modules.homeManager.shell =
