ba70671af66a9e976ed862bc75528eeb06a0d866
HarrisonCentner <harrisoncent@proton.me>
2026-07-28 22:13:30 +0000

    add age key for work

--- modules/shell/security.nix
+++ modules/shell/security.nix
@@ -1,9 +1,31 @@
+{ mkOpSecret, ... }:
+let
+  sopsAgeKeyPath = "/var/lib/opnix/secrets/sopsAgeKey";
+in
 {
-  flake.modules.homeManager.shell =
-    { pkgs, ... }:
-    {
-      home.packages = with pkgs; [
-        age
-      ];
+  _module.args.sopsAgeKeyPath = sopsAgeKeyPath;
+
+  flake.modules = {
+
+    nixos.shell = {
+      services.onepassword-secrets.secrets.sopsAgeKey = mkOpSecret {
+        service = "romeai-harrison.age";
+        field = "credential";
+        owner = "hcentner";
+        services = [ ];
+      };
     };
+
+    homeManager.shell =
+      { pkgs, ... }:
+      {
+        home = {
+          packages = with pkgs; [
+            age
+            sops
+          ];
+          sessionVariables.SOPS_AGE_KEY_FILE = sopsAgeKeyPath;
+        };
+      };
+  };
 }
