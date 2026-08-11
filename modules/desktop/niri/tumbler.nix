1c2a2a405de4f97984bb198fd999be61d3f79250
HarrisonCentner <harrisoncent@proton.me>
2026-08-11 18:49:47 +0000

    do not let thunar generate thumbnails

---
+++ modules/desktop/niri/tumbler.nix
@@ -0,0 +1,39 @@
+{
+  flake.modules.homeManager.desktop-niri =
+    { lib, ... }:
+    let
+      untrusted = [
+        "~/Downloads/untrusted"
+      ];
+
+      plugins = {
+        JPEGThumbnailer.Priority = 3;
+        RawThumbnailer.Priority = 3;
+        PixbufThumbnailer.Priority = 2;
+        CoverThumbnailer = {
+          Priority = 3;
+          Disabled = true;
+        };
+        FfmpegThumbnailer.Priority = 2;
+        GstThumbnailer.Priority = 1;
+        FontThumbnailer.Priority = 1;
+        PopplerThumbnailer.Priority = 1;
+        OdfThumbnailer.Priority = 1;
+        EpubThumbnailer.Priority = 1;
+        DesktopThumbnailer.Priority = 0;
+      };
+
+      section = name: attrs: ''
+        [${name}]
+        Disabled=${lib.boolToString (attrs.Disabled or false)}
+        Priority=${toString attrs.Priority}
+        Excludes=${lib.concatStringsSep ";" untrusted}
+        MaxFileSize=0
+      '';
+    in
+    {
+      xdg.configFile."tumbler/tumbler.rc".text = lib.concatStringsSep "\n" (
+        lib.mapAttrsToList section plugins
+      );
+    };
+}
