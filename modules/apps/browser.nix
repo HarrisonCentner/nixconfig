2448d67c082cc43574d94d5e89a2950ded7fdf76
HarrisonCentner <harrisoncent@proton.me>
2026-09-09 13:54:36 -0400

    configure tailscale-ui and proton-mail deterministically

--- modules/apps/browser.nix
+++ modules/apps/browser.nix
@@ -3,6 +3,18 @@
   flake.modules.homeManager.browser =
     { pkgs, lib, ... }:
     let
+      mkChromiumApp =
+        name: url:
+        pkgs.writeShellApplication {
+          inherit name;
+          text = ''
+            exec ${lib.getExe pkgs.ungoogled-chromium} \
+              --profile-directory=Default \
+              --app=${lib.escapeShellArg url} \
+              "$@"
+          '';
+        };
+
       chromium-novpn = pkgs.writeShellScriptBin "chromium-novpn" ''
         exec /run/wrappers/bin/mullvad-exclude \
           ${lib.getExe pkgs.ungoogled-chromium} \
@@ -10,24 +22,65 @@
           --class=chromium-novpn \
           "$@"
       '';
+
+      proton-mail = mkChromiumApp "proton-mail" "https://mail.proton.me/";
+      tailscale-ui = mkChromiumApp "tailscale-ui" "https://login.tailscale.com/admin/machines";
+
+      proton-mail-icon = pkgs.fetchurl {
+        name = "proton-mail.svg";
+        url = "https://raw.githubusercontent.com/ProtonMail/WebClients/a7c568e1de4e872644789385130aef59c6936a10/applications/mail/src/favicon.svg";
+        hash = "sha256-ks+X7lCceeS0YQrY0eD9+1N+T26eB8IzLo+Pv0uV1ME=";
+      };
+      tailscale-icon = pkgs.fetchurl {
+        name = "tailscale.svg";
+        url = "https://raw.githubusercontent.com/tailscale/tailscale/53a0d659afa51835dd7a9283873cca44261454f8/client/systray/tailscale.svg";
+        hash = "sha256-PjxdnMu1pgIfeTctGkiqQiboDgha9RQe354K03/qoNg=";
+      };
     in
     {
       # programs.firefox.enable = true;
       home.packages = [
         pkgs.ungoogled-chromium
         chromium-novpn
+        proton-mail
+        tailscale-ui
       ];
 
-      xdg.desktopEntries.chromium-novpn = {
-        name = "Chromium (no VPN)";
-        genericName = "Web Browser";
-        exec = "${lib.getExe chromium-novpn} %U";
-        icon = "chromium";
-        categories = [
-          "Network"
-          "WebBrowser"
-        ];
-        settings.StartupWMClass = "chromium-novpn";
+      xdg.desktopEntries = {
+        chromium-novpn = {
+          name = "Chromium (no VPN)";
+          genericName = "Web Browser";
+          exec = "${lib.getExe chromium-novpn} %U";
+          icon = "chromium";
+          categories = [
+            "Network"
+            "WebBrowser"
+          ];
+          settings.StartupWMClass = "chromium-novpn";
+        };
+
+        # chromium derives native Wayland app IDs from the URL and profile;
+        # matching the desktop filename gives deterministic icon association
+        "chrome-mail.proton.me__-Default" = {
+          name = "Proton Mail";
+          genericName = "Email Client";
+          exec = lib.getExe proton-mail;
+          icon = proton-mail-icon;
+          categories = [
+            "Network"
+            "Email"
+          ];
+        };
+
+        "chrome-login.tailscale.com__-Default" = {
+          name = "Tailscale";
+          genericName = "Tailscale Admin Console";
+          exec = lib.getExe tailscale-ui;
+          icon = tailscale-icon;
+          categories = [
+            "Network"
+          ];
+        };
       };
 
       xdg.mimeApps = {
@@ -39,10 +92,10 @@
         };
       };
 
-      # Proton Mail + Calendar PWAs (app-id: chrome-<webapp-id>-<profile>)
+      # keep private application windows out of screencasts
       wayland.windowManager.niri.settings._children = blockOutFromScreencast [
-        "^chrome-jnpecgipniidlgicjocehkhajgdnjekh-"
-        "^chrome-ojibjkjikcpjonjjngfkegflhmffeemk-"
+        "^chrome-mail\\.proton\\.me__-Default$"
+        "^chrome-login\\.tailscale\\.com__-Default$"
       ];
 
       ephemeralRoot.persist.directories = [
