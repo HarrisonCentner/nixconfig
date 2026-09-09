1ab98f9d560e573fdaadd9604c04908204bca306
HarrisonCentner <harrisoncent@proton.me>
2026-09-09 13:54:36 -0400

    configure tailscale-ui and proton-mail deterministically

--- modules/services/backup/immich.nix
+++ modules/services/backup/immich.nix
@@ -2,37 +2,36 @@ let
   defaultPort = 2283;
 in
 {
-  flake.modules.nixos.immich =
-    {
-      config = {
-        users.users.immich.extraGroups = [
-          "video"
-          "render"
-        ];
-        services.tailscale.serve = {
-          enable = true;
-          services.immich.endpoints = {
-            "tcp:${toString defaultPort}" = "http://localhost:${toString defaultPort}";
-          };
-        };
-        services.immich = {
-          enable = true;
-          port = defaultPort;
-          host = "0.0.0.0";
-          mediaLocation = "/var/lib/immich";
-          openFirewall = true;
-          accelerationDevices = [ "/dev/dri/renderD128" ];
-        };
-
-        ephemeralRoot.persist.directories = [ "/var/lib/immich" ];
-
-        backup = {
-          directories = [ "/var/lib/immich" ];
-          exclude = [
-            "/var/lib/immich/thumbs"
-            "/var/lib/immich/encoded-video"
-          ];
+  flake.modules.nixos.immich = {
+    config = {
+      users.users.immich.extraGroups = [
+        "video"
+        "render"
+      ];
+      services.tailscale.serve = {
+        enable = true;
+        services.immich.endpoints = {
+          "tcp:${toString defaultPort}" = "http://localhost:${toString defaultPort}";
         };
       };
+      services.immich = {
+        enable = true;
+        port = defaultPort;
+        host = "0.0.0.0";
+        mediaLocation = "/var/lib/immich";
+        openFirewall = true;
+        accelerationDevices = [ "/dev/dri/renderD128" ];
+      };
+
+      ephemeralRoot.persist.directories = [ "/var/lib/immich" ];
+
+      backup = {
+        directories = [ "/var/lib/immich" ];
+        exclude = [
+          "/var/lib/immich/thumbs"
+          "/var/lib/immich/encoded-video"
+        ];
+      };
     };
+  };
 }
